baf494c6de467542c3310df0aa5b27d1ceef27bc
Devon Germano <devon@cphandheld.com>
2018-01-16 17:49:26 +0000

    Merged PR 994: Merge fixed-scan to master
    Hopefully fixed
    
    Related work items: #2899

--- vinpointandroidapi/src/main/java/io/cphandheld/vinpoint/api/Scan.kt
+++ vinpointandroidapi/src/main/java/io/cphandheld/vinpoint/api/Scan.kt
@@ -1,8 +1,9 @@
 package io.cphandheld.vinpoint.api
 
 import android.content.Context
-import com.android.volley.Request.Method.POST
+import com.android.volley.Request.Method.GET
 import io.cphandheld.vinpoint.api.models.CPCredentials
+import io.cphandheld.vinpoint.api.models.CPInventory
 import io.cphandheld.vinpoint.api.models.CPStatusResponse
 import io.cphandheld.vinpoint.api.utility.RequestFactory
 import io.cphandheld.vinpoint.api.utility.VolleySingleton
@@ -14,8 +15,8 @@ class Scan(context: Context) {
     private val queue: VolleySingleton = VolleySingleton.getInstance(context)
     private val scannerEndpoint: String? = VinpointAPI.Environment.ScannerAPIEndpoint
 
-    fun postPrint(credentials: CPCredentials, jsonRequest: Any, statusResponse: CPStatusResponse? = null): Single<Unit> {
-        val url = "$scannerEndpoint/queue/push"
-        return RequestFactory.getSecureSingle(credentials, queue, POST, url, jsonRequest, Unit::class.java, statusResponse)
+    fun verifyVehicle(credentials: CPCredentials, vin: String, dealershipId: Int, orgId: Int, statusResponse: CPStatusResponse? = null): Single<CPInventory> {
+        val url = "$scannerEndpoint/v1/Scanner/VerifyVehicle/$vin/DealershipId/$dealershipId/OrganizationId/$orgId"
+        return RequestFactory.getSecureSingle(credentials, queue, GET, url, null, CPInventory::class.java, statusResponse)
     }
 }
