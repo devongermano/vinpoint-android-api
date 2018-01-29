2dc3ce86e6dd953f5e6a24b27d25726243ba02f9
Devon Germano <devon@cphandheld.com>
2018-01-29 21:26:21 +0000

    Merged PR 1011: Merge tag-response to master
    Tag response types were incorrect
    
    Related work items: #2924

--- vinpointandroidapi/src/main/java/io/cphandheld/vinpoint/api/Tag.kt
+++ vinpointandroidapi/src/main/java/io/cphandheld/vinpoint/api/Tag.kt
@@ -6,6 +6,7 @@ import com.android.volley.Request.Method.POST
 import io.cphandheld.vinpoint.api.models.CPCredentials
 import io.cphandheld.vinpoint.api.models.CPOrganization
 import io.cphandheld.vinpoint.api.models.CPStatusResponse
+import io.cphandheld.vinpoint.api.models.CPTag
 import io.cphandheld.vinpoint.api.utility.RequestFactory
 import io.cphandheld.vinpoint.api.utility.VolleySingleton
 import io.reactivex.Single
@@ -16,13 +17,13 @@ class Tag(context: Context) {
     private val queue: VolleySingleton = VolleySingleton.getInstance(context)
     private val endpoint: String? = VinpointAPI.Environment.APIEndpoint
 
-    fun getTags(credentials: CPCredentials, orgId: Int, statusResponse: CPStatusResponse? = null): Single<CPOrganization> {
+    fun getTags(credentials: CPCredentials, orgId: Int, statusResponse: CPStatusResponse? = null): Single<Array<CPTag>> {
         val url = "$endpoint/v1/Organizations/$orgId"
-        return RequestFactory.getSecureSingle(credentials, queue, GET, url, null, CPOrganization::class.java, statusResponse)
+        return RequestFactory.getSecureSingle(credentials, queue, GET, url, null, Array<CPTag>::class.java, statusResponse)
     }
 
-    fun upsertTags(credentials: CPCredentials, jsonRequest: Any, statusResponse: CPStatusResponse? = null): Single<Unit> {
+    fun upsertTags(credentials: CPCredentials, jsonRequest: Any, statusResponse: CPStatusResponse? = null): Single<Array<CPTag>> {
         val url = "$endpoint/v1/tags/UpsertTagsBatch"
-        return RequestFactory.getSecureSingle(credentials, queue, POST, url, jsonRequest, Unit::class.java, statusResponse)
+        return RequestFactory.getSecureSingle(credentials, queue, POST, url, jsonRequest, Array<CPTag>::class.java, statusResponse)
     }
 }
